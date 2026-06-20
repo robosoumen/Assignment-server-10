c6027459afbf50fabaf133bd8fa9857b99fc5403
soumen bera <soumenbera211@gmail.com>
2026-06-21 01:51:20 +0530

    final commit of server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+require("dotenv").config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 // TFn1ejzKxQX7UPoZ   assignment-ten
-const uri = "mongodb+srv://assignment-ten:TFn1ejzKxQX7UPoZ@cluster0.kbxs8tk.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.kbxs8tk.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
